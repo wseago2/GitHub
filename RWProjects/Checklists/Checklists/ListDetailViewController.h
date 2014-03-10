b4fb7fba20663c35646d3c4d36f0b9259c96518f
Wesley Seago <wseago2@fullsail.edu>
2014-03-10 09:13:39 -0400

    Add Checklist Navigation Controller

---
+++ RWProjects/Checklists/Checklists/ListDetailViewController.h
@@ -0,0 +1,36 @@
+//
+//  ListDetailViewController.h
+//  Checklists
+//
+//  Created by Wesley Seago on 3/10/14.
+//  Copyright (c) 2014 Wesley Seago. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ListDetailViewController;
+@class Checklist;
+
+@protocol ListDetailViewControllerDelegate <NSObject>
+
+- (void)listDetailViewControllerDidCancel:(ListDetailViewController *)controller;
+
+- (void)listDetailViewController:(ListDetailViewController *)controller
+        didFinishAddingChecklist:(Checklist *)checklist;
+
+- (void)listDetailViewController:(ListDetailViewController *)controller
+       didFinishEditingChecklist:(Checklist *)checklist;
+
+@end
+
+@interface ListDetailViewController : UITableViewController <UITextFieldDelegate>
+
+@property (nonatomic, weak) IBOutlet UITextField *textField;
+@property (nonatomic, weak) IBOutlet UIBarButtonItem *doneBarButton;
+@property (nonatomic, weak) id <ListDetailViewControllerDelegate> delegate;
+@property (nonatomic, strong) Checklist *checklistToEdit;
+
+- (IBAction)cancel;
+- (IBAction)done;
+
+@end
